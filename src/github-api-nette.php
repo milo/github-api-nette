0655ebb6ab722e2afdc47ccbae607e04e53d8620
Miloslav Hůla <miloslav.hula@gmail.com>
2014-05-26 22:31:00 +0200

    Fixed missing Messages class in manual loader.

--- src/github-api-nette.php
+++ src/github-api-nette.php
@@ -4,6 +4,7 @@ require __DIR__ . '/NetteBC/nette-bc.php';
 
 require __DIR__ . '/NetteExtension/Bridges/Cache.php';
 require __DIR__ . '/NetteExtension/Bridges/Session.php';
+require __DIR__ . '/NetteExtension/Messages.php';
 require __DIR__ . '/NetteExtension/Panel.php';
 require __DIR__ . '/NetteExtension/User.php';
 require __DIR__ . '/NetteExtension/Extension.php';
